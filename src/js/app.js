13b341bfc847451a58176e513587abde431a1b65
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-01-24 16:06:01 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
+    for (let i = 0; i < 5; i++) {
+    let newArticle = document.createElement("article");
+    newArticle.classList.add("message");
+    newArticle.innerHTML = "Sample text content " + (i+1);
+    document.body.appendChild(newArticle);
+    }
     alert("💣");
   });
 });
