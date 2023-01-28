85e7119537c1c3af33a02493ee3779a5ebac00c6
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-01-28 21:31:36 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
+    for (let i = 1; i <= 5; i++) {
     alert("💣");
     let message = document.createElement("div");
     message.classList.add("message");
-    message.innerHTML = "Sample text content";
+    message.innerHTML = "Sample text content"+i;
     document.body.appendChild(message);
+    }
   });
 });
 
