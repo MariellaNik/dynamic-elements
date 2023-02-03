c2b9dce4ce139d10b5d1d24429c23b2bb9f31dba
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-02-03 19:45:01 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,25 +3,28 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   const button = document.querySelector(".button");
   button.addEventListener("click", (event) => {
-    event.preventDefault();
-});
-});
-    let add_article=()=>{
-      const articleTemplate=document.createElement(`article`);
+  //  event.preventDefault();
+//});
+//});
+    //let add_article=()=>{
+     // const articleTemplate=document.createElement(`article`); ili
    // articleTemplate.classList.add(`message`);
-      box = document.queryselector('.message')
-      articleTemplate.innerHTML=
-        "sample text sample text sample text";
-      document.body.appendChild(articleTemplate);
-    };
+      //box = document.queryselector('.message')
+      //articleTemplate.innerHTML=
+       // "sample text sample text sample text";
+     // document.body.appendChild(articleTemplate);
+  //  };
 
-document.body.addEventListner("click", (event) => {
-  let flag=5;
-  while(flag!=0){
-    event.preventDefault();
-    add_Article();
-    flag--;
-  }
-});
-    
-   
+//document.body.addEventListner("click", (event) => {
+ // let flag=5;  while(flag!=0){    event.preventDefault();    add_Article();    flag--;  }});
+  
+    function onClick() {  
+   for (let index = 1; index <6; index++) {
+
+      let article = document.createElement('article');
+      article.className='message';
+      article.textContent='Hello '+ index;
+      main.append(article)
+   }
+    }
+  })})
