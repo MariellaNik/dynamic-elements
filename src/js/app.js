8114fa1750033b6df9ad76030530b8639913aa42
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-02-04 10:27:25 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,6 @@ import "../scss/app.scss";
     const button = document.querySelector(".button");
     button.addEventListener("click", () => {
       alert("💣");
-  
-    body.addEventListener("click", () => {
-  const body = document.querySelector('body');
-  
-    for (let i = 0; i < 5; i++) {
-      const article = document.createElement('article');
-      article.classList.add('message');
-      article.textContent = `This is message ${i + 1}`;
-      body.appendChild(article);
-    }
-  });
   });
 });
 
